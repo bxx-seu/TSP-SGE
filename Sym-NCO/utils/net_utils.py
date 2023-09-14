b20cd6425a7fded97436ad26f8d8c03f5a4d7173
bxx <17843106398@163.com>
2023-09-14 18:20:51 +0800

    first commit

---
+++ Sym-NCO/utils/net_utils.py
@@ -0,0 +1,162 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Normalization_Module(nn.Module):
+    def __init__(self, **model_params):
+        super().__init__()
+        embedding_dim = model_params['embedding_dim']
+        self.norm = nn.InstanceNorm1d(embedding_dim, affine=True, track_running_stats=False)
+    def forward(self, input):
+        # input.shape: (graph_num, nodes_num, embeddng_size)
+        transposed = input.transpose(1, 2)
+        # shape: (batch, embedding, problem)
+        normalized = self.norm(transposed)
+        # shape: (batch, embedding, problem)
+        back_trans = normalized.transpose(1, 2)
+        # shape: (batch, problem, embedding)
+        return back_trans
+
+class Add_And_Normalization_Module(nn.Module):
+    def __init__(self, **model_params):
+        super().__init__()
+        embedding_dim = model_params['embedding_dim']
+        self.norm = nn.InstanceNorm1d(embedding_dim, affine=True, track_running_stats=False)
+
+    def forward(self, input1, input2):
+        # input.shape: (batch, problem, embedding)
+
+        added = input1 + input2
+        # shape: (batch, problem, embedding)
+
+        transposed = added.transpose(1, 2)
+        # shape: (batch, embedding, problem)
+
+        normalized = self.norm(transposed)
+        # shape: (batch, embedding, problem)
+
+        back_trans = normalized.transpose(1, 2)
+        # shape: (batch, problem, embedding)
+
+        return back_trans
+
+
+class Feed_Forward_Module(nn.Module):
+    def __init__(self, **model_params):
+        super().__init__()
+        embedding_dim = model_params['embedding_dim']
+        ff_hidden_dim = model_params['ff_hidden_dim']
+
+        self.W1 = nn.Linear(embedding_dim, ff_hidden_dim)
+        self.W2 = nn.Linear(ff_hidden_dim, embedding_dim)
+
+    def forward(self, input1):
+        # input.shape: (batch, problem, embedding)
+
+        return self.W2(F.relu(self.W1(input1)))
+
+
+def get_encoding(encoded_nodes, node_index_to_pick):
+    # encoded_nodes.shape: (batch, problem, embedding)
+    # node_index_to_pick.shape: (batch, pomo)
+
+    batch_size = node_index_to_pick.size(0)
+    pomo_size = node_index_to_pick.size(1)
+    embedding_dim = encoded_nodes.size(2)
+
+    gathering_index = node_index_to_pick[:, :, None].expand(batch_size, pomo_size, embedding_dim)
+    # shape: (batch, pomo, embedding)
+
+    picked_nodes = encoded_nodes.gather(dim=1, index=gathering_index)
+    # shape: (batch, pomo, embedding)
+
+    return picked_nodes
+
+
+def reshape_by_heads(qkv, head_num):
+    # q.shape: (batch, n, head_num*key_dim)   : n can be either 1 or PROBLEM_SIZE
+
+    batch_s = qkv.size(0)
+    n = qkv.size(1)
+
+    q_reshaped = qkv.reshape(batch_s, n, head_num, -1)
+    # shape: (batch, n, head_num, key_dim)
+
+    q_transposed = q_reshaped.transpose(1, 2)
+    # shape: (batch, head_num, n, key_dim)
+
+    return q_transposed
+
+def MHA_get_weights(q, k, rank2_ninf_mask=None, rank3_ninf_mask=None):
+    batch_s = q.size(0)
+    head_num = q.size(1)
+    n = q.size(2)
+    key_dim = q.size(3)
+
+    input_s = k.size(2)
+
+    score = torch.matmul(q, k.transpose(2, 3))
+    # shape: (batch, head_num, n, problem)
+
+    score_scaled = score / torch.sqrt(torch.tensor(key_dim, dtype=torch.float))
+    if rank2_ninf_mask is not None:
+        score_scaled = score_scaled + rank2_ninf_mask[:, None, None, :].expand(batch_s, head_num, n, input_s)
+    if rank3_ninf_mask is not None:
+        score_scaled = score_scaled + rank3_ninf_mask[:, None, :, :].expand(batch_s, head_num, n, input_s)
+
+    weights = nn.Softmax(dim=3)(score_scaled)
+    # shape: (batch, head_num, n, problem)
+    return weights
+
+def multi_head_attention(q, k, v, rank2_ninf_mask=None, rank3_ninf_mask=None):
+    # q shape: (batch, head_num, n, key_dim)   : n can be either 1 or PROBLEM_SIZE
+    # k,v shape: (batch, head_num, problem, key_dim)
+    # rank2_ninf_mask.shape: (batch, problem)
+    # rank3_ninf_mask.shape: (batch, group, problem)
+
+    batch_s = q.size(0)
+    head_num = q.size(1)
+    n = q.size(2)
+    key_dim = q.size(3)
+
+    # input_s = k.size(2)
+
+    weights = MHA_get_weights(q,k,rank2_ninf_mask, rank3_ninf_mask)
+    # shape: (batch, head_num, n, problem)
+
+    out = torch.matmul(weights, v)
+    # shape: (batch, head_num, n, key_dim)
+
+    out_transposed = out.transpose(1, 2)
+    # shape: (batch, n, head_num, key_dim)
+
+    out_concat = out_transposed.reshape(batch_s, n, head_num * key_dim)
+    # shape: (batch, n, head_num*key_dim)
+
+    return out_concat
+
+def context_to_score(q,k,v, ninf_mask, encoded_nodes, multi_head_combine_W=None):
+    assert multi_head_combine_W is not None
+    out_concat = multi_head_attention(q, k, v, rank3_ninf_mask=ninf_mask)
+    # shape: (batch, pomo, head_num*qkv_dim)
+
+    mh_atten_out = multi_head_combine_W(out_concat)
+    # shape: (batch, pomo, embedding)
+
+    #  Single-Head Attention, for probability calculation
+    #######################################################
+    score = torch.matmul(mh_atten_out, encoded_nodes.transpose(1, 2))
+    # shape: (batch, pomo, problem)
+    return score
+
+def score_to_probs(score,ninf_mask, sqrt_embedding_dim, logit_clipping):
+    score_scaled = score / sqrt_embedding_dim
+    # shape: (batch, pomo, problem)
+
+    score_clipped = logit_clipping * torch.tanh(score_scaled)
+    score_masked = score_clipped + ninf_mask
+
+    probs = F.softmax(score_masked, dim=-1)
+    # shape: (batch, pomo, problem)
+
+    return probs
